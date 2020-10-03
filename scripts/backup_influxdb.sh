dee2b2c7891ac449752421a660837d9844e02f5e
Apex Connect+ <72298562+apex-connect@users.noreply.github.com>
2020-10-03 16:48:46 +0530

    Update backup_influxdb.sh

--- scripts/backup_influxdb.sh
+++ scripts/backup_influxdb.sh
@@ -2,10 +2,10 @@
 
 #first move the contents of the old backup out and clear the directory
 echo "Moving old influxdb backups if they exist"
-[ -d ~/IOTstack/backups/influxdb/db_old ] || sudo mkdir ~/IOTstack/backups/influxdb/db_old
-sudo rm ~/IOTstack/backups/influxdb/db_old/* >/dev/null 2>&1
-sudo mv ~/IOTstack/backups/influxdb/db/* ~/IOTstack/backups/influxdb/db_old/ >/dev/null 2>&1
-#sudo rm ~/IOTstack/backups/influxdb/db/*
+[ -d ~/supervised/backups/influxdb/db_old ] || sudo mkdir ~/supervised/backups/influxdb/db_old
+sudo rm ~/supervised/backups/influxdb/db_old/* >/dev/null 2>&1
+sudo mv ~/supervised/backups/influxdb/db/* ~/supervised/backups/influxdb/db_old/ >/dev/null 2>&1
+#sudo rm ~/supervised/backups/influxdb/db/*
 
 #execute the backup command
 echo "backing up influxdb database"
